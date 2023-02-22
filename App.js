cde0af3745cc934e9584153761f11a792a6e2cbe
Oluwatomisin <oluwatomisinoyebanji@gmail.com>
2023-02-22 11:12:53 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App(){
+    return (
+        <div>
+            <Header />
+            Working
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
